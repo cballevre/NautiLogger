44b31d98e1c2cf9f53511d623729814767f8a97e
cballevre <celestin.ballevre@protonmail.com>
2025-07-03 17:22:35 +0200

    feat: Add authentification with Refine

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Refine } from "@refinedev/core";
+import { Refine, Authenticated } from "@refinedev/core";
 import { dataProvider } from "@refinedev/supabase";
 
 import { supabaseClient } from "./utils/supabaseClient";
+import { authProvider } from "./providers/auth-provider.ts";
 import { ShowBoat } from "./pages/boats/show";
 
 function App() {
 
   return (
     <Refine
-      dataProvider={dataProvider(supabaseClient)}>
-      <ShowBoat />
+      dataProvider={dataProvider(supabaseClient)}
+      authProvider={authProvider}>
+      <Authenticated key="protected" fallback={<div>Not authenticated</div>}>
+        <ShowBoat />
+      </Authenticated>
     </Refine>
   )
 }
