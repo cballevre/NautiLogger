759d20058991d0e291a81018f800a807a3fc48a8
cballevre <celestin.ballevre@protonmail.com>
2025-07-04 13:16:33 +0200

    feat: Add Ant Design and default login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,64 @@
 import { Refine, Authenticated } from "@refinedev/core";
 import { dataProvider } from "@refinedev/supabase";
-import routerProvider from "@refinedev/react-router";
-import { Routes, Outlet, Route, Navigate } from "react-router";
+import routerProvider, {
+  NavigateToResource
+} from "@refinedev/react-router";
+import { Routes, Outlet, Route } from "react-router";
+import { ConfigProvider, App as AntdApp } from "antd";
+import { AuthPage } from "@refinedev/antd";
+import { BrowserRouter } from "react-router";
 
 import { supabaseClient } from "./utils/supabaseClient";
 import { authProvider } from "./providers/auth-provider.ts";
 import { ListBoat } from "./pages/boats/list.tsx";
 
-import { Login } from "./pages/login.tsx";
 
 function App() {
 
   return (
-    <Refine
-      dataProvider={dataProvider(supabaseClient)}
-      authProvider={authProvider}
-      routerProvider={routerProvider}>
-        <Routes>
-          <Route
-            element={
-              // We're wrapping our routes with the `<Authenticated />` component
-              // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
-              // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
-              <Authenticated key="authenticated-routes" redirectOnFail="/login">
-                <Outlet />
-              </Authenticated>
-            }
-          >
-            <Route index element={<ListBoat />} />
-          </Route>
-          <Route
-            element={
-              <Authenticated key="auth-pages" fallback={<Outlet />}>
-                {/* We're redirecting the user to `/` if they are authenticated and trying to access the `/login` route */}
-                <Navigate to="/" />
-              </Authenticated>
-            }
-          >
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-    </Refine>
+    <BrowserRouter>
+      <ConfigProvider>
+        <AntdApp>
+          <Refine
+            dataProvider={dataProvider(supabaseClient)}
+            authProvider={authProvider}
+            routerProvider={routerProvider}>
+              <Routes>
+                <Route
+                  element={
+                    // We're wrapping our routes with the `<Authenticated />` component
+                    // We're omitting the `fallback` prop to redirect users to the login page if they are not authenticated.
+                    // If the user is authenticated, we'll render the `<Header />` component and the `<Outlet />` component to render the inner routes.
+                    <Authenticated key="authenticated-routes" redirectOnFail="/login">
+                      <Outlet />
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<ListBoat />} />
+                </Route>
+                <Route
+                  element={
+                    <Authenticated key="auth-pages" fallback={<Outlet />}>
+                      <NavigateToResource />
+                    </Authenticated>
+                  }
+                >
+                  <Route path="/login" element={<AuthPage />} />
+                  <Route path="/register" element={<AuthPage type="register" />} />
+                  <Route
+                    path="/forgot-password"
+                    element={<AuthPage type="forgotPassword" />}
+                  />
+                  <Route
+                    path="/update-password"
+                    element={<AuthPage type="updatePassword" />}
+                  />
+                </Route>
+              </Routes>
+          </Refine>
+        </AntdApp>
+      </ConfigProvider>
+    </BrowserRouter>
   )
 }
 
