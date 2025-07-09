176453589b6bdcb1d470199e8a32a4c51e42546d
cballevre <celestin.ballevre@protonmail.com>
2025-07-09 16:18:20 +0200

    feat: Polish boat list with card display

--- src/pages/boats/list.tsx
+++ src/pages/boats/list.tsx
@@ -1,4 +1,6 @@
-import { Link, useList, useTranslation } from '@refinedev/core';
+import { PlusOutlined } from '@ant-design/icons';
+import { useGo, useList, useTranslation } from '@refinedev/core';
+import { Card, Col, Row } from 'antd';
 
 const ListBoat = () => {
   const { data: boats } = useList({
@@ -6,18 +8,51 @@ const ListBoat = () => {
   });
 
   const { translate } = useTranslation();
+  const go = useGo();
 
   return (
     <div>
       <h1>{translate('ListBoat.title')}</h1>
-      <Link to="/boats/add">{translate('ListBoat.add')}</Link>
-      <ul>
+      <Row gutter={16}>
         {boats?.data?.map((boat) => (
-          <li key={boat.id}>
-            <Link to={`/boats/${boat.id}`}>{boat.name}</Link>
-          </li>
+          <Col span={6} key={boat.id}>
+            <Card
+              hoverable
+              style={{
+                aspectRatio: 2 / 1,
+                margin: '10px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                textAlign: 'center',
+              }}
+              onClick={() => {
+                go({ to: `/boats/${boat.id}/dashboard` });
+              }}
+            >
+              <Card.Meta title={boat.name} description={boat.description} />
+            </Card>
+          </Col>
         ))}
-      </ul>
+        <Col span={6}>
+          <Card
+            hoverable
+            style={{
+              aspectRatio: 2 / 1,
+              margin: '10px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+            onClick={() => {
+              go({ to: '/boats/add' });
+            }}
+          >
+            <PlusOutlined style={{ fontSize: '32px', color: '#555' }} />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
