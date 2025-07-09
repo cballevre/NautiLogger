f6e86523ef6a7b6c0150c24089978f9946c73c2c
cballevre <celestin.ballevre@protonmail.com>
2025-07-09 15:48:45 +0200

    refactor: Extract string into translation files

--- src/pages/boats/add.tsx
+++ src/pages/boats/add.tsx
@@ -1,28 +1,30 @@
-import { useGetIdentity } from '@refinedev/core';
-import { useForm, Create } from '@refinedev/antd';
+import { Create, useForm } from '@refinedev/antd';
+import { useGetIdentity, useTranslation } from '@refinedev/core';
 import { Form, Input } from 'antd';
 
 const AddBoat = () => {
-  const { data: identity } = useGetIdentity();
+  const { data: identity } = useGetIdentity<{ id: string }>();
 
   const { formProps, saveButtonProps, onFinish } = useForm({
     resource: 'boats',
     action: 'create',
   });
 
-  const handleOnFinish = (values) => {
+  const handleOnFinish = (values: {}) => {
     onFinish({
       owner_id: identity?.id,
       ...values,
     });
   };
 
+  const { translate } = useTranslation();
+
   return (
     <div>
-      <h1>Add a new boat</h1>
+      <h1>{translate('AddBoat.title')}</h1>
       <Create saveButtonProps={saveButtonProps}>
         <Form {...formProps} onFinish={handleOnFinish} layout="vertical">
-          <Form.Item label="Name" name="name">
+          <Form.Item label={translate('AddBoat.labels.name')} name="name">
             <Input />
           </Form.Item>
         </Form>
