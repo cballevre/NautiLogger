e4f807aab44e186dbc21a284fb0357173584c97d
cballevre <celestin.ballevre@protonmail.com>
2025-07-09 11:10:52 +0200

    feat: Allow user to create their own boat

--- src/pages/boats/list.tsx
+++ src/pages/boats/list.tsx
@@ -1,14 +1,18 @@
-import { useList } from "@refinedev/core";
+import { useList, Link } from "@refinedev/core";
 
 const ListBoat = () => {
 
     const { data: boats } = useList({
         resource: "boats",
     });
+
     return (
         <div>
             <h1>Boat List</h1>
             <p> Showing {boats?.total} records in total. </p>
+            <Link to="/boats/add">
+                Create a new boat
+            </Link>
             <ul>
                 {boats?.data?.map((boat) => (
                     <li key={boat.id}>
