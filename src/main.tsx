e674e450bb252d6b592e39de32d6eab459ee68e1
cballevre <celestin.ballevre@protonmail.com>
2025-07-09 12:41:19 +0200

    refactor: Rename file to be less case sensitive

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './global.css';
-import App from './App.tsx';
+import App from './app.tsx';
 import './utils/i18n';
 
 const root = document.getElementById('root');
