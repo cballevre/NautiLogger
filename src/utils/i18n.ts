5215384bb08685b292489db8e522ae9bb07713fe
cballevre <celestin.ballevre@protonmail.com>
2025-07-16 12:02:28 +0200

    style: Reorganize and harmonize import

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
 import detector from 'i18next-browser-languagedetector';
+import backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(Backend)
+  .use(backend)
   .use(detector)
   .use(initReactI18next)
   .init({
