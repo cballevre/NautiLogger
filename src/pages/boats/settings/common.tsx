96a3aaac23a92509727d3dcfa32b0021eea7f211
cballevre <celestin.ballevre@protonmail.com>
2025-07-16 11:10:00 +0200

    feat: Add possibility to delete a boat

--- src/pages/boats/settings/common.tsx
+++ src/pages/boats/settings/common.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from '@refinedev/core';
+import { Button, Typography } from 'antd';
 
 import { SettingsLayout } from '@/components/settings-layout';
 
@@ -7,7 +8,27 @@ const CommonSettings = () => {
 
   return (
     <SettingsLayout title={translate('CommonSettings.title')}>
-      <p>Manage your boat's general settings here.</p>
+      <section>
+        <header>
+          <Typography.Title level={3}>
+            {translate('CommonSettings.dangerZone.title')}
+          </Typography.Title>
+          <Typography.Paragraph>
+            {translate('CommonSettings.dangerZone.description')}
+          </Typography.Paragraph>
+        </header>
+        <div>
+          <Button
+            type="primary"
+            danger
+            onClick={() => {
+              // Handle the delete action here
+            }}
+          >
+            Delete boat
+          </Button>
+        </div>
+      </section>
     </SettingsLayout>
   );
 };
