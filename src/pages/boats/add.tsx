79e5092d201f25df7ab5cd1e558653668014956f
cballevre <celestin.ballevre@protonmail.com>
2025-07-21 15:27:06 +0200

    feat(AddBoat): Add layout to center content

--- src/pages/boats/add.tsx
+++ src/pages/boats/add.tsx
@@ -2,6 +2,8 @@ import { Create, useForm } from '@refinedev/antd';
 import { useGetIdentity, useTranslation } from '@refinedev/core';
 import { Form, Input } from 'antd';
 
+import { PageLayout } from '@/components/page-layout';
+
 const AddBoat = () => {
   const { data: identity } = useGetIdentity<{ id: string }>();
 
@@ -20,16 +22,18 @@ const AddBoat = () => {
   const { translate } = useTranslation();
 
   return (
-    <Create
-      saveButtonProps={saveButtonProps}
-      title={translate('AddBoat.title')}
-    >
-      <Form {...formProps} onFinish={handleOnFinish} layout="vertical">
-        <Form.Item label={translate('AddBoat.labels.name')} name="name">
-          <Input />
-        </Form.Item>
-      </Form>
-    </Create>
+    <PageLayout>
+      <Create
+        saveButtonProps={saveButtonProps}
+        title={translate('AddBoat.title')}
+      >
+        <Form {...formProps} onFinish={handleOnFinish} layout="vertical">
+          <Form.Item label={translate('AddBoat.labels.name')} name="name">
+            <Input />
+          </Form.Item>
+        </Form>
+      </Create>
+    </PageLayout>
   );
 };
 
