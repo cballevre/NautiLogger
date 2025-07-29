bfc37afbba2fc100ef3d71fdc19bbfc7a804fb44
cballevre <celestin.ballevre@protonmail.com>
2025-07-29 21:08:46 +0200

    feat: Seeding more equipments to make tests easier

--- supabase/seeds/seed.ts
+++ supabase/seeds/seed.ts
@@ -2,6 +2,8 @@ import { copycat } from '@snaplet/copycat';
 import { createSeedClient } from '@snaplet/seed';
 import bcrypt from 'bcryptjs';
 
+import { boatSystemList } from '../../src/models/boat-system.ts';
+
 const hashPassword = async (password: string) => {
   const saltRounds = 6;
   const salt = await bcrypt.genSalt(saltRounds);
@@ -15,48 +17,51 @@ const main = async () => {
   // Truncate all tables in the database
   await seed.$resetDatabase();
 
+  const authenticatedUser = {
+    instance_id: '00000000-0000-0000-0000-000000000000',
+    aud: 'authenticated',
+    role: 'authenticated',
+    email_confirmed_at: new Date(),
+    raw_app_meta_data: {
+      provider: 'email',
+      providers: ['email'],
+    },
+    raw_user_meta_data: {
+      email_verified: true,
+    },
+    banned_until: null,
+  };
+
   const { users } = await seed.users([
     {
-      instance_id: '00000000-0000-0000-0000-000000000000',
-      aud: 'authenticated',
-      role: 'authenticated',
       email: 'loick.perron@example.com',
       encrypted_password: await hashPassword('password'),
-      email_confirmed_at: new Date(),
-      raw_app_meta_data: {
-        provider: 'email',
-        providers: ['email'],
-      },
-      raw_user_meta_data: {
-        email_verified: true,
-      },
-      banned_until: null,
+      ...authenticatedUser,
     },
     {
-      aud: 'authenticated',
-      role: 'authenticated',
       email: 'alain.gautier@example.com',
       encrypted_password: await hashPassword('password'),
-      email_confirmed_at: new Date(),
-      raw_app_meta_data: {
-        provider: 'email',
-        providers: ['email'],
-      },
-      raw_user_meta_data: {
-        email_verified: true,
-      },
-      banned_until: null,
+      ...authenticatedUser,
     },
   ]);
 
-  await seed.boats(
+  const { boats } = await seed.boats(
     (x) =>
       x(4, ({ seed }) => ({
         name: copycat.firstName(seed),
         interventions: [{}, {}],
-        equipments: [{}, {}],
       })),
     { connect: { users } },
+  );
+
+  await seed.equipments(
+    (x) =>
+      x(50, ({ seed }) => ({
+        name: copycat.lastName(seed),
+        system_key:
+          boatSystemList[Math.floor(Math.random() * boatSystemList.length)],
+      })),
+    { connect: { boats } },
   );
 
   process.exit();
