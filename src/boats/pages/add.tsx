84bf7e079bcb389dc938fbdceb282f5a9cc2641f
cballevre <celestin.ballevre@protonmail.com>
2025-07-30 19:12:18 +0200

    fix: Redirect to list view after boat creation

--- src/boats/pages/add.tsx
+++ src/boats/pages/add.tsx
@@ -1,18 +1,33 @@
 import { Create, useForm } from '@refinedev/antd';
-import { useGetIdentity, useTranslate } from '@refinedev/core';
+import { useGetIdentity, useGo, useTranslate } from '@refinedev/core';
 import { Form, Input } from 'antd';
 
 import { PageLayout } from '@/shared/components/page-layout';
 
 const AddBoat = () => {
   const { data: identity } = useGetIdentity<{ id: string }>();
+  const go = useGo();
 
   const { formProps, saveButtonProps, onFinish } = useForm({
     resource: 'boats',
     action: 'create',
+    redirect: false,
+    successNotification: () => ({
+      type: 'success',
+      message: translate('boats.add.notification.success.message'),
+      description: translate('boats.add.notification.success.description'),
+    }),
+    errorNotification: () => ({
+      type: 'error',
+      message: translate('boats.add.notification.error.message'),
+      description: translate('boats.add.notification.error.description'),
+    }),
+    onMutationSuccess: () => {
+      go({ to: '/boats', type: 'replace' });
+    },
   });
 
-  const handleOnFinish = (values: {}) => {
+  const handleOnFinish = (values: Record<string, unknown>) => {
     onFinish({
       created_by: identity?.id,
       ...values,
@@ -25,10 +40,19 @@ const AddBoat = () => {
     <PageLayout>
       <Create
         saveButtonProps={saveButtonProps}
-        title={translate('AddBoat.title')}
+        title={translate('boats.add.title')}
       >
         <Form {...formProps} onFinish={handleOnFinish} layout="vertical">
-          <Form.Item label={translate('AddBoat.labels.name')} name="name">
+          <Form.Item
+            label={translate('boats.add.form.labels.name')}
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: translate('boats.add.form.validation.name_required'),
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
         </Form>
