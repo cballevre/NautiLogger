43a12d08dab2d986760e897f395be3d39e157dac
cballevre <celestin.ballevre@protonmail.com>
2025-07-31 08:30:09 +0200

    feat(PageHeader): Add back button

--- src/shared/components/page-header.tsx
+++ src/shared/components/page-header.tsx
@@ -1,48 +1,68 @@
-import { Grid, Typography } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { useBack } from '@refinedev/core';
+import { Button, Grid, Typography } from 'antd';
 import type { FC } from 'react';
 
 interface PageHeaderProps {
   title: string;
   subtitle?: string;
   actions?: React.ReactNode;
+  back?: string;
 }
 
 const { useBreakpoint } = Grid;
 
-const PageHeader: FC<PageHeaderProps> = ({ title, subtitle, actions }) => {
+const PageHeader: FC<PageHeaderProps> = ({
+  title,
+  subtitle,
+  actions,
+  back,
+}) => {
   const screens = useBreakpoint();
+  const backFn = useBack();
 
   return (
-    <header
-      style={{
-        display: 'flex',
-        flexDirection: screens.sm ? 'row' : 'column',
-        justifyContent: 'space-between',
-        alignItems: screens.sm ? 'center' : 'flex-start',
-        marginBottom: '24px',
-      }}
-    >
-      <div>
-        <Typography.Title
-          level={1}
-          style={{
-            marginBlockStart: 0,
-            fontSize: '20px',
-            lineHeight: 1.4,
-            marginBlockEnd: 0,
-          }}
+    <header style={{ marginBottom: '24px' }}>
+      {back ? (
+        <Button
+          type="link"
+          style={{ marginBottom: '16px' }}
+          onClick={() => backFn()}
+          icon={<ArrowLeftOutlined />}
         >
-          {title}
-        </Typography.Title>
-        {subtitle ? (
-          <Typography.Text style={{ color: '#555555' }}>
-            {subtitle}
-          </Typography.Text>
+          {back}
+        </Button>
+      ) : null}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: screens.sm ? 'row' : 'column',
+          justifyContent: 'space-between',
+          alignItems: screens.sm ? 'center' : 'flex-start',
+        }}
+      >
+        <div>
+          <Typography.Title
+            level={1}
+            style={{
+              marginBlockStart: 0,
+              fontSize: '20px',
+              lineHeight: 1.4,
+              marginBlockEnd: 0,
+            }}
+          >
+            {title}
+          </Typography.Title>
+          {subtitle ? (
+            <Typography.Text style={{ color: '#555555' }}>
+              {subtitle}
+            </Typography.Text>
+          ) : null}
+        </div>
+        {actions ? (
+          <div style={{ marginTop: screens.sm ? 0 : '16px' }}>{actions}</div>
         ) : null}
       </div>
-      {actions ? (
-        <div style={{ marginTop: screens.sm ? 0 : '16px' }}>{actions}</div>
-      ) : null}
     </header>
   );
 };
